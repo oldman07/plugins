0901e96b6620be02b7c65f9af8dcc720d467a2d6
oldman07 <backagain010@gmail.com>
2024-07-17 10:43:04 +0500

    add admin -menu+ seperate menu code

--- social_media_plugin1/index.php
+++ social_media_plugin1/index.php
@@ -6,9 +6,11 @@ Version: 1.0
 Author: Your Name
 */
 
+// Include the settings page file
+include_once(plugin_dir_path(__FILE__) . '/include/social-media-main.php');
+
 // Enqueue the plugin's stylesheet
 function social_media_links_enqueue_styles() {
-    
     wp_enqueue_style('social-media-links-style', plugins_url('style.css', __FILE__));
 }
 add_action('wp_enqueue_scripts', 'social_media_links_enqueue_styles');
@@ -27,11 +29,27 @@ function social_media_links_shortcode($atts) {
     $html = '<div class="social-media-links">';
     foreach ($atts as $platform => $link) {
         if (!empty($link)) {
-            $html.= "<a href='{$link}' target='_blank'>{$platform}</a>";
+            $html .= "<a href='{$link}' target='_blank'>{$platform}</a>";
         }
     }
-    $html.= '</div>';
+    $html .= '</div>';
 
     return $html;
 }
 add_shortcode('social_media_links', 'social_media_links_shortcode');
+
+
+
+// Function to add the menu item
+function social_media_links_add_menu_item() {
+    add_menu_page(
+        'Social Media Links Settings', // Page title
+        'Social Media Links', // Menu title
+        'manage_options', // Capability required to access the page
+        'social-media-links-settings', // Menu slug
+        'social_media_links_settings_page', // Function that renders the page
+        'dashicons-share-alt', // Icon URL
+        6 // Position in the menu; lower numbers correspond with higher positions
+    );
+}
+add_action('admin_menu', 'social_media_links_add_menu_item');
